d45df1151f1469076e3bf4e6881835d48d2ae2a5
pauloohaha <85469000+pauloohaha@users.noreply.github.com>
2023-08-13 02:58:21 +0800

    modify debug at score add

--- src/coll_score/ucc_coll_score.c
+++ src/coll_score/ucc_coll_score.c
@@ -81,7 +81,9 @@ coll_score_add_range(ucc_coll_score_t *score, ucc_coll_type_t coll_type,
         return UCC_ERR_INVALID_PARAM;
     }
 
-    ucc_info("***score add: team:%s, score:%d\n",team->context->lib->log_component.name, msg_score);
+    if(team->rank == 0){
+        ucc_info("***score add: team:%s, score:%d\n, mtype:%d, coll type:%d",team->context->lib->log_component.name, msg_score, mem_type, coll_type);
+    }
     return UCC_OK;
 }
 ucc_status_t ucc_coll_score_add_range(ucc_coll_score_t *score,
