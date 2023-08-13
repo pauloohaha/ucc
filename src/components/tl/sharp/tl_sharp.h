4154271cbd2bea9865e080e826c1b6c094c6dfb3
Piao DENG <85469000+pauloohaha@users.noreply.github.com>
2023-08-13 17:24:03 +0800

    Revert "RSV"
    This reverts commit 84175a88dab07d928b09f4bb01f2432dc7830a18.

--- src/components/tl/sharp/tl_sharp.h
+++ src/components/tl/sharp/tl_sharp.h
@@ -136,7 +136,7 @@ ucc_status_t sharp_status_to_ucc_status(int status);
 #define TASK_ARGS(_task) (_task)->super.bargs.args
 
 #define UCC_TL_SHARP_SUPPORTED_COLLS                                           \
-    (UCC_COLL_TYPE_ALLREDUCE | UCC_COLL_TYPE_BARRIER | UCC_COLL_TYPE_BCAST | UCC_COLL_TYPE_REDUCE_SCATTERV)
+    (UCC_COLL_TYPE_ALLREDUCE | UCC_COLL_TYPE_BARRIER | UCC_COLL_TYPE_BCAST | UCC_COLL_TYPE_REDUCE_SCATTER)
 
 UCC_CLASS_DECLARE(ucc_tl_sharp_team_t, ucc_base_context_t *,
                   const ucc_base_team_params_t *);
